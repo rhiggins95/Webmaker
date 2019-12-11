3ddfa24b20e59d25c70bce7a768e332af16154a3
Christine Brown <cbrown2@roxbury.edu>
2019-12-11 16:08:28 -0500

    11/25

--- client/src/App.js
+++ client/src/App.js
@@ -55,18 +55,97 @@ const [users, setUsers] =useState([
   firstName: "ronelle", 
   lastName: "Higgins", 
   email: "swang@ulem.org"
+  }
+]);
+const [websites, setWebsites] = useState([
+  {_id: "123", name: "Facebook", developerId: "456", description: "Lorem" },
+  {_id: "234", name: "Twitter", developerId: "456", description: "Lorem" },
+  {_id: "456", name: "MSIMBO", developerId: "456", description: "Lorem" },
+  {_id: "890", name: "Go", developerId: "123", description: "Lorem" },
+{
+  _id: "567",
+  name: "Tic Tac Toe",
+  developerId: "123",
+  description: "Lorem"
+},
+ {_id: "678", name: "Checkers", developerId: "123", description: "Lorem" },
+ {_id: "789", name: "Chess", developerId: "234", description: "Lorem" },
+]);
+
+// Add a new user into users
+const addUser = user => {
+  setUsers([...users, user]);
+};
+
+  // update user by id
+  const updateUser = newUser => {
+    setUsers(
+      users.map(user => {
+      if (user._id === newUser._id) {
+        return newUser;
+      } else {
+        return user;
+      }
+})
+);
+  };
+
+// get websites by user id
+const getWebsites = uid => {
+  const curWebs = [];
+  for (let website of websites) {
+    if (website.developerId === uid) {
+      curWebs.pusg(website);
+    }
+  }
 }
-  ]);
+
+const getWebsite = wid => {
+  for (let website of websites) {
+    if ( website._id === wid) {
+      return website;
+    }
+  }
+};
+
+// add new website
+const addWebsite = newWeb => {
+  setWebsites([...websites, newWeb]);
+};
+
+// remove website
+const removeWebsite = wid => {
+  setWebsites(websites.filter(website => website._id !== wid));
+};
+
+  //  update website
+  const updateWebsite = newWeb => {
+  setWebsites(
+  websites.map(website => {
+  if (website._id === newWeb._id) {
+  return newWeb;
+  } else {
+return website; 
+}
+})
+  );
+};
 
   return ( 
    <Router>
       <Switch>
         <Route exact path="/login"> <Login users={users} /> </Route>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/user/:uid" component={Profile}/>
-        <Route exact path="/user/:uid/website" component={WebsiteList}/>
-        <Route exact path="/user/:uid/website/new" component={WebsiteNew}/>
-        <Route exact path="/user/:uid/website/:wid" component={WebsiteEdit}/>
+        <Route exact path="/register"> <Register users={users} addUser={addUser} /> </Route>
+        <Route exact path="/user/:uid"> <Profile users={users} updateUser={updateUser} /></Route>
+        <Route exact path="/user/:uid/website"> <WebsiteList getWebsites={getWebsites} /></Route>
+        <Route exact path="/user/:uid/website/new"> <WebsiteNew getWebsites={getWebsites} /></Route>
+        <Route exact path="/user/:uid/website/:wid"> <WebsiteEdit 
+        getWebsites={getWebsites} 
+        getWebsite={getWebsite} 
+        removeWebsite={removeWebsite}
+        updateWebsite={updateWebsite}
+        />
+        </Route>
         <Route exact path="/user/:uid/website/:wid/page" component={PageList}/>
         <Route exact path="/user/:uid/website/:wid/page/new" component={PageNew}/>
         <Route exact path="/user/:uid/website/:wid/page/:pid" component={PageEdit}/>
