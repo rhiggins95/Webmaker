73d73ee3be8107d8b28d5ed1c981da78fbad8ad6
Ronelle Higgins <ronellethiggins@gmail.com>
2020-02-07 21:28:19 -0500

    link to mlab

---
+++ client/build/precache-manifest.1e47f1ce32fd6b2e0c07371134876a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d10626c2dfc79fe93d58e880f93eff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c77581c2b5b3ff36b0",
+    "url": "/static/css/main.dc72e114.chunk.css"
+  },
+  {
+    "revision": "1273081ef9f28df47ea8",
+    "url": "/static/js/2.17f09653.chunk.js"
+  },
+  {
+    "revision": "75c77581c2b5b3ff36b0",
+    "url": "/static/js/main.a3f6f5cb.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
